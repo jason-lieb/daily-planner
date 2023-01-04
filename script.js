8adc2eaf961d7fe798c5e8867c51bb3f19cba8e3
Jason Lieb <Jason.lieb@outlook.com>
2023-01-04 14:15:21 -0500

    create time blocks and add functions to style with background color based on past, present, and future

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 $(function () {
+  init();
+
+  function init() {
+    let timeBlocks = $('.time-block');
+    $.each(timeBlocks, (i) => {
+      let time = $(timeBlocks[i]).attr('id').split('-')[1];
+      $(timeBlocks[i]).addClass(checkTime(time))
+    })
+  }
+
+  function checkTime(time) {
+    let current = dayjs().hour();
+    if (time < current) {
+      return 'past'
+    } else if (time > current) {
+      return 'future'
+    } else {
+      return 'present'
+    }
+  }
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
